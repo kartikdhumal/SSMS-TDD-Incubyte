cf1630387f7ba4a5ba1c25f9b7bc11b3df61d5ce
Kartik Dhumal <kartik.dhumal@lucentinnovation.com>
2025-12-13 22:13:07 +0530

    test: add failing tests for search sweets

---
+++ server/tests/sweets/searchSweet.test.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../../app');
+const mongoose = require('mongoose');
+const connectDB = require('../../config/db');
+const Sweet = require('../../models/sweets.model');
+const User = require('../../models/user.model');
+
+let adminToken;
+
+beforeAll(async () => {
+    await connectDB();
+});
+
+beforeAll(async () => {
+  await User.deleteMany({});
+  await Sweet.deleteMany({});
+
+  const adminRes = await request(app).post('/api/auth/register').send({
+    name: 'Admin',
+    email: 'admin@test.com',
+    password: '123456',
+    role: 'admin',
+  });
+
+  adminToken = adminRes.body.token;
+
+  await Sweet.insertMany([
+    { name: 'Ladoo', category: 'Milk', price: 50, quantity: 10 },
+    { name: 'Barfi', category: 'Milk', price: 100, quantity: 5 },
+    { name: 'Jalebi', category: 'Sugar', price: 30, quantity: 8 },
+  ]);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe('GET /api/sweets/search', () => {
+  it('should search sweets by name', async () => {
+    const res = await request(app).get('/api/sweets/search?name=ladoo');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(1);
+  });
+
+  it('should search sweets by category', async () => {
+    const res = await request(app).get('/api/sweets/search?category=Milk');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(2);
+  });
+
+  it('should search sweets by price range', async () => {
+    const res = await request(app).get('/api/sweets/search?minPrice=40&maxPrice=80');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(1);
+  });
+
+  it('should return empty array if no sweets found', async () => {
+    const res = await request(app).get('/api/sweets/search?name=Nothing');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual([]);
+  });
+});
